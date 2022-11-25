0adea20b6394648d3591aad66e9f080e0d2f23ec
latttysh <semyon.latysh@gmail.com>
2022-11-25 17:14:30 +0300

    Архитектура

---
+++ src/Admin/Admin.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const Admin: FunctionComponent<Props> = (props) => {
+
+    //Логика для админа
+  return (
+      <div>
+
+      </div>
+  );
+};
+
+export default Admin;
---
+++ src/Client/Pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const LandingPage: FunctionComponent<Props> = (props) => {
+  // Лендинг
+  return (
+      <div></div>
+  );
+};
+
+export default LandingPage;
