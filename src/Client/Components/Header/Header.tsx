8266b262c27e38acc483aaf9327765c64ffe5362
WDIH404 <flazy322@gmail.com>
2022-12-10 16:48:02 +0700

    THE END

--- src/Client/Components/Header/Header.tsx
+++ src/Client/Components/Header/Header.tsx
@@ -1,55 +1,57 @@
-import React, {FunctionComponent} from 'react';
-import s from "./header.module.scss"
-import {ReactComponent as Logotype} from "../../Assets/img/logo.svg";
-import {ReactComponent as USA} from "../../Assets/img/usa.svg";
-import PrimaryButton from "../Buttons/PrimaryButton/PrimaryButton";
-import {NavLink, useNavigate} from "react-router-dom";
+import React, { FunctionComponent, useState } from 'react';
+import s from './header.module.scss';
+import { ReactComponent as Logotype } from '../../Assets/img/logo.svg';
+import { ReactComponent as USA } from '../../Assets/img/usa.svg';
+import PrimaryButton from '../Buttons/PrimaryButton/PrimaryButton';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
-interface OwnProps {
-}
+interface OwnProps {}
 
 type Props = OwnProps;
 
 const Header: FunctionComponent<Props> = (props) => {
+  const navigate = useNavigate();
+  const [nav, setNav] = useState(false);
 
-    const navigate = useNavigate()
+  return (
+    <div className={s.header}>
+      <div className={s.menu}>
+        <NavLink to={'/'}>
+          <div className={s.logotype}>
+            <Logotype />
+          </div>
+        </NavLink>
 
-    return (
-        <div className={s.header}>
-            <div className={s.menu}>
-                <NavLink to={"/"}>
-                    <div className={s.logotype}>
-                        <Logotype/>
-                    </div>
-                </NavLink>
-
-                <div className={s.navigation}>
-                    <NavLink to={"/portfolio"}>
-                        <div className={s.navigation_item}>Наши работы</div>
-                    </NavLink>
-                    <NavLink to={"/blog"}>
-                        <div className={s.navigation_item}>Блог</div>
-                    </NavLink>
-                    <NavLink to={"/about"}>
-                        <div className={s.navigation_item}>О нас</div>
-                    </NavLink>
-                    <NavLink to={"/contacts"}>
-                        <div className={s.navigation_item}>Контакты</div>
-                    </NavLink>
-
-                </div>
-            </div>
-            <div className={s.actions}>
-                <div className={s.language}>
-                    <div className={s.language_flag}>
-                        <USA/>
-                    </div>
-                    <div className={s.language_text}>switch language</div>
-                </div>
-                <PrimaryButton title={"Начать проект"} action={() => navigate("/send")}/>
-            </div>
+        <div className={nav ? `${s.navigation} ${s.active}` : s.navigation} >
+          <NavLink to={'/portfolio'}>
+            <div className={s.navigation_item}>Наши работы</div>
+          </NavLink>
+          <NavLink to={'/blog'}>
+            <div className={s.navigation_item}>Блог</div>
+          </NavLink>
+          <NavLink to={'/about'}>
+            <div className={s.navigation_item}>О нас</div>
+          </NavLink>
+          <NavLink to={'/contacts'}>
+            <div className={s.navigation_item}>Контакты</div>
+          </NavLink>
         </div>
-    );
+      </div>
+      <div className={s.actions}>
+        <div className={s.language}>
+          <div className={s.language_flag}>
+            <USA />
+          </div>
+          <div className={s.language_text}>switch language</div>
+        </div>
+        <PrimaryButton title={'Начать проект'} action={() => navigate('/send')} />
+        <div onClick={() => setNav(!nav)} className={s.burger_menu}>
+          {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
